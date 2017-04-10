d769ef01c4771779269b21241f1ed0e4e4f5425f
russellomo <russellomo@dhcp-168-105-29-21.wireless.manoa.hawaii.edu>
2017-04-10 09:56:44 -1000

    Removed routing, placed style elements in respective page

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -2,35 +2,57 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 FlowRouter.route('/', {
-  name: 'Landing_Page',
+  name: 'Home_Page',
   action() {
-    BlazeLayout.render('App_Body', { main: 'Landing_Page' });
+    BlazeLayout.render('App_Body', { main: 'Home_Page' });
   },
 });
 
-FlowRouter.route('/list', {
-  name: 'List_Stuff_Page',
+//Item routes
+FlowRouter.route('/item', {
+  name: 'Item_Page',
   action() {
-    BlazeLayout.render('App_Body', { main: 'List_Stuff_Page' });
+    BlazeLayout.render('App_Body', { main: 'Item_Page' });
   },
 });
 
-FlowRouter.route('/add', {
-  name: 'Add_Stuff_Page',
+FlowRouter.route('/item-feed', {
+  name: 'Item_Feed_Page',
   action() {
-    BlazeLayout.render('App_Body', { main: 'Add_Stuff_Page' });
+    BlazeLayout.render('App_Body', { main: 'Item_Feed_Page' });
   },
 });
 
-FlowRouter.route('/stuff/:_id', {
-  name: 'Edit_Stuff_Page',
+FlowRouter.route('/create-item', {
+  name: 'Create_Item_Page',
   action() {
-    BlazeLayout.render('App_Body', { main: 'Edit_Stuff_Page' });
+    BlazeLayout.render('App_Body', { main: 'Create_Item_Page' });
   },
 });
 
-FlowRouter.notFound = {
-  action() {
-    BlazeLayout.render('App_Body', { main: 'App_Not_Found' });
-  },
-};
+
+/* ORIGINAL EXAMPLES
+ FlowRouter.route('/list', {
+ name: 'List_Stuff_Page',
+ action() {
+ BlazeLayout.render('App_Body', { main: 'List_Stuff_Page' });
+ },
+ });
+ FlowRouter.route('/add', {
+ name: 'Add_Stuff_Page',
+ action() {
+ BlazeLayout.render('App_Body', { main: 'Add_Stuff_Page' });
+ },
+ });
+ FlowRouter.route('/stuff/:_id', {
+ name: 'Edit_Stuff_Page',
+ action() {
+ BlazeLayout.render('App_Body', { main: 'Edit_Stuff_Page' });
+ },
+ });
+ FlowRouter.notFound = {
+ action() {
+ BlazeLayout.render('App_Body', { main: 'App_Not_Found' });
+ },
+ };
+ */
