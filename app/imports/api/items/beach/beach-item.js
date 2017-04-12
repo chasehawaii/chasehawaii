41377b40c131211252a890c3560ab6864c70e5ac
Your Name <you@example.com>
2017-04-12 07:05:38 -1000

    Update
    Created the collections files for the items (hikes, restaurants,
    beaches). Still need to add functions in the files to define new
    instances.

--- app/imports/api/items/beach/beach-item.js
+++ app/imports/api/items/beach/beach-item.js
@@ -5,48 +5,46 @@ import { Meteor } from 'meteor/meteor';
 
 class BeachCollection extends BaseCollection {
   /**
-   Creates the Hike Items collection
+   Creates the Beach Items schema
    */
   constructor() {
-    super('Hike', new SimpleSchema({
+    super('Beach', new SimpleSchema({
       title: {
         label: 'title',
         type: String,
         optional: false,
       },
       category: {
+        label: 'category',
         type: String,
         optional: false,
       },
       location: {
+        label: 'location',
         type: String,
       },
       status: {
+        label: 'status',
         type: String,
         optional: false,
       },
       about: {
-        type: String
+        label: 'about',
+        type: String,
       },
       tags: {
-        type: String
+        label: 'tags',
+        type: String,
       },
-      kind: {
-        type: String
+      picture: {
+        type: SimpleSchema.RegEx.Url,
+        optional: true,
       },
-      length: {
-        type: String
-      },
-      difficulty: {
-        type: String
-      },
-
     }));
-
   }
-
-
-
-
-
 }
+
+/**
+ * Provides the singleton instance of this class to all other entities.
+ */
+export const Beaches = new BeachCollection();
---
+++ app/imports/api/items/restaurant/index.js
@@ -0,0 +1,2 @@
+import './restaurant-item.js';
+
