24d0716c450bfe877d84c1c9cbdce51a2ad70251
russellomo <russellomo@p1-00-00-03.a0.bvt00.adelphiacom.net>
2017-04-15 17:53:17 -1000

    First commit for the issue

---
+++ app/imports/api/profiles/ProfileCollection.js
@@ -0,0 +1,74 @@
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+/* eslint-disable object-shorthand */
+
+export const Profiles = new Mongo.Collection('Profiles');
+
+/**
+ * Create the schema for Stuff
+ */
+export const ProfilesSchema = new SimpleSchema({
+  username: {
+    label: 'username',
+    type: String,
+    optional: false,
+    max: 20,
+  },
+  first: {
+    label: 'first',
+    type: String,
+    optional: false,
+    max: 20,
+  },
+  last: {
+    label: 'last',
+    type: String,
+    optional: false,
+    max: 20,
+  },
+  standing: {
+    label: 'standing',
+    type: String,
+    optional: false,
+    max: 20,
+  },
+  /*
+  facebook: {
+    label: 'facebook',
+    type: String,
+    optional: true,
+    max: 20,
+  },
+  insta: {
+    label: 'insta',
+    type: String,
+    optional: true,
+    max: 20,
+    autoform: {
+      group: 'Stuff',
+      placeholder: '3',
+    },
+  },
+  twitter: {
+    label: 'twitter',
+    type: String,
+    optional: true,
+    max: 20,
+  },
+  */
+  image: {
+    label: 'image',
+    type: String,
+    optional: true,
+    max: 200,
+  },
+  about: {
+    label: 'about',
+    type: String,
+    optional: false,
+    max: 140,
+  },
+});
+
+Profiles.attachSchema(ProfilesSchema);
