02122b2f00ef94d0ebc6df34cfa50a1931617956
idemello <idemello@hawaii.edu>
2017-04-30 11:06:31 -1000

    checkpoint

--- app/imports/ui/pages/items/beach-page.js
+++ app/imports/ui/pages/items/beach-page.js
@@ -13,3 +13,27 @@ Template.Beach_Page.helpers({
 
 });
 
+Template.Beach_Page.events({
+  'submit .beach-comment-form'(event, instance) {
+    event.preventDefault();
+    // Get name (text field)
+    const username = Meteor.user().profile.name;
+    const content = event.target.Content.value;
+
+    const newItemData = { username,  content };
+
+    // Clear out any old validation errors.
+    instance.context.resetValidation();
+    // Invoke clean so that newStudentData reflects what will be inserted.
+    ProfilesSchema.clean(newItemData);
+    // Determine validity.
+    instance.context.validate(newItemData);
+    if (instance.context.isValid()) {
+      Profiles.insert(newItemData);
+      instance.messageFlags.set(displayErrorMessages, false);
+      FlowRouter.go(`/${username}/profile`);
+    } else {
+      instance.messageFlags.set(displayErrorMessages, true);
+    }
+  },
+});
