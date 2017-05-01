5c76004e5dba3114d881c35244378101d807d9f5
idemello <idemello@hawaii.edu>
2017-05-01 12:38:51 -1000

    checkpoint, submit not adding to collection.

--- app/imports/ui/pages/items/beach-page.js
+++ app/imports/ui/pages/items/beach-page.js
@@ -1,6 +1,8 @@
 import './beach-page.html';
 import { Template } from 'meteor/templating';
 import { Beaches } from '/imports/api/items/beach/beach-item.js';
+import { Comments, CommentsSchema } from '/imports/api/comments/CommentsCollection.js';
+import { Meteor } from 'meteor/meteor';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
 Template.Beach_Page.onCreated(function onCreated() {
@@ -10,6 +12,9 @@ Template.Beach_Page.onCreated(function onCreated() {
 Template.Beach_Page.helpers({
   bea: () => Beaches.findOne({ _id: FlowRouter.getParam('_id') }),
 
+  CommentsList() {
+    return Comments.find();
+  },
 
 });
 
@@ -19,21 +24,18 @@ Template.Beach_Page.events({
     // Get name (text field)
     const username = Meteor.user().profile.name;
     const content = event.target.Content.value;
-
-    const newItemData = { username,  content };
+    const newItemData = { username, content };
 
     // Clear out any old validation errors.
     instance.context.resetValidation();
     // Invoke clean so that newStudentData reflects what will be inserted.
-    ProfilesSchema.clean(newItemData);
+    CommentsSchema.clean(newItemData);
     // Determine validity.
     instance.context.validate(newItemData);
-    if (instance.context.isValid()) {
-      Profiles.insert(newItemData);
-      instance.messageFlags.set(displayErrorMessages, false);
-      FlowRouter.go(`/${username}/profile`);
-    } else {
-      instance.messageFlags.set(displayErrorMessages, true);
-    }
+
+    Comments.insert(newItemData);
+    instance.messageFlags.set(displayErrorMessages, false);
+    FlowRouter.reload();
+
   },
 });
