3325c72af547bfffb7217e6108da6c5774dcd126
idemello <idemello@hawaii.edu>
2017-05-02 09:23:20 -1000

    fixed console errors

--- app/imports/ui/pages/items/beach-page.js
+++ app/imports/ui/pages/items/beach-page.js
@@ -4,9 +4,12 @@ import { Beaches } from '/imports/api/items/beach/beach-item.js';
 import { Comments, CommentsSchema } from '/imports/api/comments/CommentsCollection.js';
 import { Meteor } from 'meteor/meteor';
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { _ } from 'meteor/underscore';
 
 Template.Beach_Page.onCreated(function onCreated() {
   this.subscribe('Beaches');
+  this.context = CommentsSchema.namedContext('Beach_Page')
 });
 
 Template.Beach_Page.helpers({
@@ -23,8 +26,8 @@ Template.Beach_Page.events({
     event.preventDefault();
     // Get name (text field)
     const username = Meteor.user().profile.name;
-    const content = event.target.Content.value;
-    const newItemData = { username, content };
+    const about = event.target.About.value;
+    const newItemData = { username, about };
 
     // Clear out any old validation errors.
     instance.context.resetValidation();
@@ -34,7 +37,7 @@ Template.Beach_Page.events({
     instance.context.validate(newItemData);
 
     Comments.insert(newItemData);
-    instance.messageFlags.set(displayErrorMessages, false);
+  //  instance.messageFlags.set(displayErrorMessages, false);
     FlowRouter.reload();
 
   },
