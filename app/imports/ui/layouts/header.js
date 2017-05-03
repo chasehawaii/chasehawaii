c68b130b550463d1a9e22cf9c93307c22b066f7a
idemello <idemello@hawaii.edu>
2017-05-02 21:07:35 -1000

    does not properly redirect on click

--- app/imports/ui/layouts/header.js
+++ app/imports/ui/layouts/header.js
@@ -1,7 +1,21 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { _ } from 'meteor/underscore';
+import { Meteor } from 'meteor/meteor';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Profiles, ProfilesSchema } from '/imports/api/profiles/ProfileCollection.js';
 
 // The Header menu does not use dropdown menus, but most menus do.
 // Here's how to do the required initialization for Semantic UI dropdown menus.
 Template.Header.onRendered(function enableDropDown() {
   this.$('.dropdown').dropdown();
+});
+
+Template.Header.events({
+
+  'click img': function () {
+    //const username = Meteor.user().profile.name;
+    FlowRouter.go(`/${Meteor.user().profile.name}/profile`);
+  }
+
 });
